47838bc42aa0df35b7e27bed1c05ebed09ec82bf
WincerChan <WincerChan@gmail.com>
2020-07-07 14:22:36 +0800

    :white_check_mark: add: benchmark for ral test

--- lib/ral_cell.ex
+++ lib/ral_cell.ex
@@ -1,38 +1,22 @@
 defmodule Ral.Cell do
-  use GenServer
   alias :ets, as: ETS
 
   @total Application.get_env(:ral, :total)
   @speed Application.get_env(:ral, :speed)
   @member Application.get_env(:ral, :member)
 
-  @doc "start Ral.Cell"
-  @spec init(any) :: {:ok, nil}
-  def init(_) do
-    Ral.Clear.start()
-    {:ok, nil}
-  end
-
-  @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
-  def start_link(_) do
-    IO.puts("Starting ral...")
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
-  end
-
   @spec choke(any) :: boolean
   def choke(key) do
     lookup(key)
-    |> calcu_rest()
+    |> calc_rest()
     |> allow?(key)
   end
 
-  @spec calcu_rest({number, DateTime.t()}) :: {float, DateTime.t()}
-  defp calcu_rest({rest, prev, d_score?}) do
+  defp calc_rest({rest, prev, d_score?}) do
     n = DateTime.utc_now()
     {DateTime.diff(n, prev, :millisecond) * @speed / 1000 + rest - 1, n, d_score?, prev}
   end
 
-  @spec lookup(any) :: {Integer, DateTime.t()}
   defp lookup(key) do
     case ETS.lookup(@member, key) do
       [{_, prev, rest}] -> {rest, prev, true}
@@ -42,8 +26,8 @@ defmodule Ral.Cell do
 
   defp allow?({rest, now, d_score?, prev}, key) do
     new_rest = min(rest, @total) |> round()
-    send(:clear, {:delete, d_score?, {prev, key}})
-    send(:clear, {:insert, key, now, if(rest <= 0, do: 0, else: new_rest)})
+    send(Ral.CMD, {:delete, d_score?, {prev, key}})
+    send(Ral.CMD, {:insert, key, now, if(rest <= 0, do: 0, else: new_rest)})
     if rest <= 0, do: false, else: true
   end
 end
--- test/ral_cell_test.exs
+++ test/ral_cell_test.exs
@@ -1,25 +1,40 @@
 defmodule Ral.CellTest do
   use ExUnit.Case
 
+  # defp benchmark(_, 0), do: nil
+
+  # defp benchmark(key, times) do
+  #   Ral.Cell.choke(key)
+  #   benchmark(key, times - 1)
+  # end
+
+  defp permutations([]), do: [[]]
+
+  defp permutations(list),
+    do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
+
   test "do test succeed" do
-    assert Ral.Cell.choke(:two) == true
+    assert Ral.Cell.choke(:live) == true
   end
 
-  @doc """
-  please don't add --cover in test command.
-  """
-  test "do test benchmark..." do
-    fun = fn
-      _, _, 0 ->
-        nil
+  defp test_benchmark(0), do: nil
 
-      fun, key, times ->
-        Ral.Cell.choke(key)
-        fun.(fun, key, times - 1)
-    end
+  defp test_benchmark(n) do
+    pms =
+      permutations(["a", "b", "c", "d", "e", "f", "g", "h", "i"])
+      |> Enum.map(&to_string/1)
+      |> Enum.map(&String.to_atom/1)
 
-    {elapsed, _} = :timer.tc(fn -> fun.(fun, :one, 100_000) end)
-    IO.puts(" Test 100_000 times Ral.Cell.choke/1 elapsed #{elapsed / 1000_000} s")
-    assert Ral.Cell.choke(:one) == false
+    {elapsed, _} =
+      :timer.tc(fn ->
+        pms |> Enum.map(&Ral.Cell.choke/1)
+      end)
+
+    IO.puts(" Test#{n} #{length(pms)} times Ral.Cell.choke/1 elapsed #{elapsed / 1000_000} s")
+    test_benchmark(n - 1)
+  end
+
+  test "do test benchmark ..." do
+    test_benchmark(10)
   end
 end
